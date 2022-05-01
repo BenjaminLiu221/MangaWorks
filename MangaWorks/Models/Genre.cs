17f4b80b7e0c0b48f3e7b67972fb2cd028d48145
Benjamin Liu <benjaminliu221@gmail.com>
2022-05-01 15:46:15 -0700

    first commit

---
+++ MangaWorks/Models/Genre.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MangaWorks.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
