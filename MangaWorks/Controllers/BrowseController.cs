56b12170b63493e5efad9ef8ded3ea3f848a4bcf
Benjamin Liu <benjaminliu221@gmail.com>
2022-05-02 00:12:15 -0700

    validated delete genre method

--- MangaWorks/Controllers/BrowseController.cs
+++ MangaWorks/Controllers/BrowseController.cs
@@ -41,5 +41,68 @@ namespace MangaWorks.Controllers
             }
             return View(genreObj);
         }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var genreFromDb = _dbContext.Genres.Find(id);
+            if (genreFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(genreFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Genre genreObj)
+        {
+            if (genreObj.Name == genreObj.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.Genres.Update(genreObj);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(genreObj);
+        }
+
+        //GET
+        public IActionResult Remove(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var genreFromDb = _dbContext.Genres.Find(id);
+            if (genreFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(genreFromDb);
+        }
+
+        //POST
+        [HttpPost,ActionName("Remove")]
+        [ValidateAntiForgeryToken]
+        public IActionResult RemovePOST(int? id)
+        {
+            var genreObj = _dbContext.Genres.Find(id);
+            if (genreObj == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Genres.Remove(genreObj);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
