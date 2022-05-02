2f1f8e4d72756835a56c1d58435fccff089629b6
Benjamin Liu <benjaminliu221@gmail.com>
2022-05-01 21:12:58 -0700

    added server side and client side validation

--- MangaWorks/Controllers/BrowseController.cs
+++ MangaWorks/Controllers/BrowseController.cs
@@ -17,5 +17,29 @@ namespace MangaWorks.Controllers
             IEnumerable<Genre> objGenreList = _dbContext.Genres;
             return View(objGenreList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Genre genreObj)
+        {
+            if (genreObj.Name == genreObj.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.Genres.Add(genreObj);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(genreObj);
+        }
     }
 }
