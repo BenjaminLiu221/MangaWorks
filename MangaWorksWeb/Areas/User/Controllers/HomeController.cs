3ac1f735392e517d0c259e5a8d3771c70b7b5f7b
Benjamin Liu <benjaminliu221@gmail.com>
2022-05-08 17:54:45 -0700

    work on homepage

--- MangaWorksWeb/Areas/User/Controllers/HomeController.cs
+++ MangaWorksWeb/Areas/User/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using MangaWorks.Models;
+﻿using MangaWorks.DataAccess.Repository.IRepository;
+using MangaWorks.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +9,19 @@ namespace MangaWorksWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Manga> mangaList = _unitOfWork.Manga.GetAll(includeProperties: "Genre,Author");
+
+            return View(mangaList);
         }
 
         public IActionResult Privacy()
