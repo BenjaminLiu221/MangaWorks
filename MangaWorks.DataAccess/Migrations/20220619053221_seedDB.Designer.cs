e06b960a11ff467bb6f330f6897d359c3c629abf
Benjamin Liu <benjaminliu221@gmail.com>
2022-06-18 22:33:17 -0700

    fixed issue caused by installing EF 6

--- MangaWorks.DataAccess/Migrations/20220619053221_seedDB.Designer.cs
+++ MangaWorks.DataAccess/Migrations/20220619053221_seedDB.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MangaWorks.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20220619011316_seedDB")]
+    [Migration("20220619053221_seedDB")]
     partial class seedDB
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
