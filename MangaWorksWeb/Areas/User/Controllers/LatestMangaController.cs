4b540bef2304493124315f3cfd4dce0b4791a840
Benjamin Liu <benjaminliu221@gmail.com>
2022-06-25 02:06:28 -0700

    added latest manga controller

---
+++ MangaWorksWeb/Areas/User/Controllers/LatestMangaController.cs
@@ -0,0 +1,44 @@
+﻿using MangaWorks.DataAccess.Repository.IRepository;
+using MangaWorks.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MangaWorksWeb.Controllers
+{
+    [Area("User")]
+    public class LatestMangaController : Controller
+    {
+        private readonly ILogger<LatestMangaController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        public LatestMangaController(ILogger<LatestMangaController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var mangaList = _unitOfWork.Manga.GetAll(includeProperties: "Author").ToList();
+            var lastChapterList = new List<Chapter>();
+            var latestMangaList = new List<Manga>();
+
+            foreach (var manga in mangaList)
+            {
+                var lastChapterOfManga = _unitOfWork.Chapter.GetAll().Where(a => a.MangaId == manga.Id).OrderBy(a => a.ChapterNumber).LastOrDefault();
+                lastChapterList.Add(lastChapterOfManga);
+            }
+
+            List<Chapter> orderedLastChapterList = lastChapterList.OrderByDescending(a => a.Updated).ToList();
+            
+            foreach(var chapter in orderedLastChapterList)
+            {
+                latestMangaList.Add(_unitOfWork.Manga.GetFirstOrDefault(a => a.Id == chapter.MangaId, includeProperties: "Author"));
+            }
+
+            LatestManga latestManga = new()
+            {
+                MangaList = latestMangaList,
+            };
+            return View(latestManga);
+        }
+    }
+}
