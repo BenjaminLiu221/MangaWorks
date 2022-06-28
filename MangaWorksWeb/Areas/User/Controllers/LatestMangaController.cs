aa2ce8996404b90799f1c127eedad8a46073f7cb
Benjamin Liu <benjaminliu221@gmail.com>
2022-06-27 21:29:35 -0700

    added carousel to latestmanga index

--- MangaWorksWeb/Areas/User/Controllers/LatestMangaController.cs
+++ MangaWorksWeb/Areas/User/Controllers/LatestMangaController.cs
@@ -21,22 +21,47 @@ namespace MangaWorksWeb.Controllers
             var lastChapterList = new List<Chapter>();
             var latestMangaList = new List<Manga>();
 
+            var mostPopularMangaList = new List<Manga>();
+            var mangaListByVotesDesc = _unitOfWork.Manga.GetAll(includeProperties: "Author").OrderByDescending(a => a.NumberOfRatings).ToList();
+            var topWeekMangaList = new List<Manga>();
+
             foreach (var manga in mangaList)
             {
                 var lastChapterOfManga = _unitOfWork.Chapter.GetAll().Where(a => a.MangaId == manga.Id).OrderBy(a => a.ChapterNumber).LastOrDefault();
-                lastChapterList.Add(lastChapterOfManga);
+                if (lastChapterOfManga != null)
+                {
+                    lastChapterList.Add(lastChapterOfManga);
+                }
             }
 
-            List<Chapter> orderedLastChapterList = lastChapterList.OrderByDescending(a => a.Updated).ToList();
+            List<Chapter> descendingLastChapterList = lastChapterList.OrderByDescending(a => a.Updated).ToList();
             
-            foreach(var chapter in orderedLastChapterList)
+            foreach(var chapter in descendingLastChapterList)
             {
                 latestMangaList.Add(_unitOfWork.Manga.GetFirstOrDefault(a => a.Id == chapter.MangaId, includeProperties: "Author"));
+            }
+
+            foreach (var manga in mangaListByVotesDesc)
+            {
+                var lastChapterOfManga = _unitOfWork.Chapter.GetAll().Where(a => a.MangaId == manga.Id).OrderByDescending(a => a.ChapterNumber).FirstOrDefault();
+                if (lastChapterOfManga != null)
+                {
+                    lastChapterList.Add(lastChapterOfManga);
+                }
+            }
+
+            //topWeekMangaList
+            List<Chapter> orderedLastChapterList = lastChapterList.OrderByDescending(a => a.Updated).ToList();
+
+            foreach (var chapter in orderedLastChapterList)
+            {
+                topWeekMangaList.Add(_unitOfWork.Manga.GetFirstOrDefault(a => a.Id == chapter.MangaId, includeProperties: "Author"));
             }
 
             LatestManga latestManga = new()
             {
                 MangaList = latestMangaList,
+                TopWeekManga = topWeekMangaList,
             };
             return View(latestManga);
         }
