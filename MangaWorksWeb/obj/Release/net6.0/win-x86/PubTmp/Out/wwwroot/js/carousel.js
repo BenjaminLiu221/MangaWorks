0b571ac5ff77356632b4a6a67b5415604c04e7fe
Benjamin Liu <benjaminliu221@gmail.com>
2022-07-10 04:14:07 -0700

    updated carousel js to a ready function

--- MangaWorksWeb/obj/Release/net6.0/win-x86/PubTmp/Out/wwwroot/js/carousel.js
+++ MangaWorksWeb/obj/Release/net6.0/win-x86/PubTmp/Out/wwwroot/js/carousel.js
@@ -1,8 +1,19 @@
-﻿$('.autoplay').slick({
+﻿////$('.autoplay').slick({
+////    slidesToShow: 8,
+////    slidesToScroll: 1,
+////    autoplay: true,
+////    dots: false,
+////    arrows: true,
+////    autoplaySpeed: 2000,
+////});
+
+$(document).ready(function () {
+    $('.autoplay').slick({
     slidesToShow: 8,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     dots: false,
     arrows: true,
     autoplaySpeed: 2000,
+    });
 });
